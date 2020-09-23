de9895b98db30c30d955f3b231e6caccef2c67cd
Tanuj Mathur <tmathur@thoughtworks.com>
2020-09-24 01:35:16 +0200

    Add app skeleton and test libraries

---
+++ go.mod
@@ -0,0 +1,5 @@
+module go.kata.com/semver
+
+go 1.14
+
+require github.com/stretchr/testify v1.6.1
---
+++ semver_test.go
@@ -0,0 +1,13 @@
+package semver
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_CreatePodSpec(t *testing.T) {
+	t.Run("should fail", func(t *testing.T) {
+		assert.Fail(t, "FAILED!!!")
+	})
+}
